c2afe8f49fb866f9ca68921601ef7a9dd3c9722b
ALitch0 <leathheal@gmail.com>
2024-03-23 21:27:44 -0400

    created models

--- api/index.js
+++ api/index.js
@@ -11,7 +11,10 @@ mongoose.connect(process.env.CONNECTION_STRING).then(()=>console.log("Database C
     err;
 })
 
+const productsController = require('./controllers/productsController');
+
+app.use('/api/products', productsController)
+
 app.listen(9000,()=>{
     console.log('Listening in port 9000');
 })
-//MGFT3n5RHLGCzoeD
